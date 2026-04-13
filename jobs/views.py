a16e591b6c215a86375ba44e9003c7fd588aee49
Patricio De Maria <108100239+pppato@users.noreply.github.com>
2026-04-13 20:27:30 -0300

    feat: templates lista y detalles de jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Job
 # Create your views here.
+class JobListView(ListView):
+    model = Job
+    template_name = "job_list.html"
+    context_object_name = "jobs"
+
+class JobDetailView(DetailView):
+    model = Job
+    template_name = "job_detail.html"
+    context_object_name = "job"
